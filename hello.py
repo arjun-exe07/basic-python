a67fdba043c97a6000d449e35e1c3259ffe077e8
arjun-exe07 <arjunkadam202007@gmail.com>
2026-04-22 22:33:32 +0530

    add hello.py

---
+++ hello.py
@@ -0,0 +1,12 @@
+def main():
+  hello('Arjun')
+  night('Mayur')
+
+def hello(name):
+  print(f"Hello, {name}")
+
+def night(name):
+  print(f"good night, {name}")
+
+if __int__ == "__main__":
+  main()
